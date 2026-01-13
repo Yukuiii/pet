b2d89785adbabf2c9909b33515c1ad1ee2709101
Yukuiii <472619563@qq.com>
2026-01-13 10:59:23 +0800

    init

---
+++ pet-backend/src/main/java/pet/PetBackendApplication.java
@@ -0,0 +1,13 @@
+package pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetBackendApplication.class, args);
+    }
+
+}
